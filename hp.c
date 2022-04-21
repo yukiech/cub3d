649861de1b83448b9ed5537103495670c99d7eb9
42ahuber <alain.huber91@gmail.com>
2022-04-21 16:53:28 +0200

    hp test

--- hp.c
+++ hp.c
@@ -27,7 +27,7 @@ void	ft_hp_draw(t_vars *vars)
 			mlx_put_image_to_window(vars->mlx, vars->win, vars->loading.heart_half.img, 720, 30);
 		if (vars->player.hp * 100 / vars->player.hp_start > 87)
 			mlx_put_image_to_window(vars->mlx, vars->win, vars->loading.heart.img, 720, 30);
-		mlx_string_put(vars->mlx, vars->win, 600, 30, 0x000000, "HP : ");
-		mlx_string_put(vars->mlx, vars->win, 650, 30, 0x000000, ft_itoa(vars->player.hp));
+//		mlx_string_put(vars->mlx, vars->win, 600, 30, 0x000000, "HP : ");
+//		mlx_string_put(vars->mlx, vars->win, 650, 30, 0x000000, ft_itoa(vars->player.hp));
 	}
 }
