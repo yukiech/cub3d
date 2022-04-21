bb4abc50531881cf30e9b8eb07767b6296bae4f1
42ahuber <alain.huber91@gmail.com>
2022-04-21 10:50:25 +0200

    menu update & sound 3

--- loading_menu.c
+++ loading_menu.c
@@ -2,12 +2,16 @@
 
 void	menu_load_image(t_vars *vars)
 {	
-	ft_load_image(vars, "./assets/menu_loading.xpm", &vars->loading.loading);
-	ft_load_image(vars, "./assets/menu_loading2.xpm", &vars->loading.loading2);
+	ft_load_image(vars, "./assets/menu_1.xpm", &vars->loading.menu_1);
+	ft_load_image(vars, "./assets/menu_2.xpm", &vars->loading.menu_2);
+	ft_load_image(vars, "./assets/menu_3.xpm", &vars->loading.menu_3);
+	ft_load_image(vars, "./assets/menu_4.xpm", &vars->loading.menu_4);
 	ft_load_image(vars, "./assets/char_ghost.xpm", &vars->loading.ghost);
 	ft_load_image(vars, "./assets/char_ninja.xpm", &vars->loading.ninja);
 	ft_load_image(vars, "./assets/char_pirate.xpm", &vars->loading.pirate);
 	ft_load_image(vars, "./assets/char_franken.xpm", &vars->loading.franken);
+	ft_load_image(vars, "./assets/sound_on.xpm", &vars->loading.sound_on);
+	ft_load_image(vars, "./assets/sound_off.xpm", &vars->loading.sound_off);
 }
 
 void	set_char_stats(t_vars *vars)
@@ -26,10 +30,18 @@ void	loading_screen(t_vars *vars, int i)
 {
 	if (i == 0)
 	{
-		if (vars->loading.i % 100 > 90)
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->loading.loading.img, 0, 0);
-		else
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->loading.loading2.img, 0, 0);
+		if (vars->loading.i % 100 < 18)
+			mlx_put_image_to_window(vars->mlx, vars->win, vars->loading.menu_1.img, 0, 0);
+		else if (vars->loading.i % 100 > 17 && vars->loading.i % 100 < 35)
+			mlx_put_image_to_window(vars->mlx, vars->win, vars->loading.menu_2.img, 0, 0);
+		else if (vars->loading.i % 100 > 34 && vars->loading.i % 100 < 52)
+			mlx_put_image_to_window(vars->mlx, vars->win, vars->loading.menu_3.img, 0, 0);
+		else if (vars->loading.i % 100 > 51 && vars->loading.i % 100 < 69)
+			mlx_put_image_to_window(vars->mlx, vars->win, vars->loading.menu_4.img, 0, 0);
+		else if (vars->loading.i % 100 > 68 && vars->loading.i % 100 < 86)
+			mlx_put_image_to_window(vars->mlx, vars->win, vars->loading.menu_3.img, 0, 0);
+		else if (vars->loading.i % 100 > 85)
+			mlx_put_image_to_window(vars->mlx, vars->win, vars->loading.menu_2.img, 0, 0);
 	vars->loading.i += 1;
 	}
 	else if (i == 1)
@@ -40,5 +52,9 @@ void	loading_screen(t_vars *vars, int i)
 		mlx_put_image_to_window(vars->mlx, vars->win, vars->loading.pirate.img, 0, 0);
 	else if (i == 4)
 		mlx_put_image_to_window(vars->mlx, vars->win, vars->loading.franken.img, 0, 0);
+	if (vars->loading.sound == 0 && vars->game_state == 1)
+		mlx_put_image_to_window(vars->mlx, vars->win, vars->loading.sound_off.img, 720, 30);
+	else if (vars->loading.sound == 1 && vars->game_state == 1)
+		mlx_put_image_to_window(vars->mlx, vars->win, vars->loading.sound_on.img, 720, 30);
 	return;
 }
